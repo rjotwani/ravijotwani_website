4b36162ec6c961fa8132116e73383352d2a88341
rjotwani <ravi.jotwani@jakra.net>
2018-10-13 17:17:45 -0700

    created homepage and added resume

---
+++ js/content.js
@@ -0,0 +1,29 @@
+/*
+ * HOME
+ */
+
+ var i = 0;
+ var txt = "Hi, I'm Ravi.";
+ var speed = 75;
+
+ function typeWriter() {
+   if (i < txt.length) {
+     document.getElementById("typewriter").innerHTML += txt.charAt(i);
+     i++;
+     setTimeout(typeWriter, speed);
+   }
+ }
+
+ typeWriter();
+
+/*
+ * RESUME
+ */
+
+var h = window.innerHeight
+|| document.documentElement.clientHeight
+|| document.body.clientHeight;
+
+document.getElementById("main").height =
+document.getElementById("resume").height =
+document.getElementById("embeddedFile").height = h;
--- js/resume.js
+++
@@ -1,7 +0,0 @@
-var h = window.innerHeight
-|| document.documentElement.clientHeight
-|| document.body.clientHeight;
-
-document.getElementById("main").height =
-document.getElementById("resume").height =
-document.getElementById("embeddedFile").height = h;
