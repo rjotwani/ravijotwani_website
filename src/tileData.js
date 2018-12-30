e113b5e2001fa6e4a3704b0f401a1d982719cb57
rjotwani <ravi.jotwani@jakra.net>
2018-12-29 18:14:56 -0800

    changed link styling, added portfolio template, updated links

---
+++ src/tileData.js
@@ -0,0 +1,70 @@
+const tileData = [
+  {
+    img: './images/breakfast.jpg',
+    title: 'Breakfast',
+    author: 'jill111',
+    cols: 2,
+    featured: true,
+  },
+  {
+    img: './images/burgers.jpg',
+    title: 'Tasty burger',
+    author: 'director90',
+  },
+  {
+    img: './images/camera.jpg',
+    title: 'Camera',
+    author: 'Danson67',
+  },
+  {
+    img: './images/morning.jpg',
+    title: 'Morning',
+    author: 'fancycrave1',
+    featured: true,
+    href: 'https://google.com'
+  },
+  {
+    img: './images/hats.jpg',
+    title: 'Hats',
+    author: 'Hans',
+  },
+  {
+    img: './images/honey.jpg',
+    title: 'Honey',
+    author: 'fancycravel',
+  },
+  {
+    img: './images/vegetables.jpg',
+    title: 'Vegetables',
+    author: 'jill111',
+    cols: 2,
+  },
+  {
+    img: './images/plant.jpg',
+    title: 'Water plant',
+    author: 'BkrmadtyaKarki',
+  },
+  {
+    img: './images/mushroom.jpg',
+    title: 'Mushrooms',
+    author: 'PublicDomainPictures',
+  },
+  {
+    img: './images/olive.jpg',
+    title: 'Olive oil',
+    author: 'congerdesign',
+  },
+  {
+    img: './images/star.jpg',
+    title: 'Sea star',
+    cols: 2,
+    author: '821292',
+  },
+  {
+    img: './images/bike.jpg',
+    title: 'Bike',
+    author: 'danfador',
+  },
+];
+
+export default tileData;
